d9174126f00e443a85259502941dbc80af96bbbe
김예진 <kimyejin0044@gmail.com>
2025-05-02 13:32:00 +0900

    Revert "Merge branch 'dev-and' into 'develop'"
    This reverts merge request !2

--- spicoAndroid/app/src/main/java/com/a401/spicoandroid/SpicoApp.kt
+++
@@ -1,7 +0,0 @@
-package com.a401.spicoandroid
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SpicoApp : Application() {}
--- spicoAndroid/app/src/main/java/com/a401/spicoandroid/common/domain/HandledException.kt
+++
@@ -1,6 +0,0 @@
-package com.a401.spicoandroid.common.domain
-
-class HandledException(
-    userMessage: String,
-    cause: Throwable? = null
-) : Exception(userMessage, cause)
