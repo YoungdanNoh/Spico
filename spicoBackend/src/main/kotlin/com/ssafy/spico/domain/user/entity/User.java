c404cdba73c903541bde11f8fa803bcfc554e54d
이승연 <snowball@sookmyung.ac.kr>
2025-05-07 14:28:23 +0900

    ♻️[S12P31A401-166] refactor: Update User entity

--- spicoBackend/src/main/kotlin/com/ssafy/spico/domain/user/entity/User.java
+++ spicoBackend/src/main/kotlin/com/ssafy/spico/domain/user/entity/User.java
@@ -13,10 +13,44 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String email;
+
     private String name;
 
-    // 필요한 생성자만 작성
-    public User(String name) {
+    private boolean hasAudience;
+
+    private boolean hasQna;
+
+    private Integer questionCount;
+
+    private Integer answerTimeLimit;
+
+    public User(String email, String name) {
+        this.email = email;
         this.name = name;
+        this.hasAudience = true;
+        this.hasQna = true;
+        this.questionCount = 1;
+        this.answerTimeLimit = 1;
+    }
+
+    public void updateHasAudience(boolean hasAudience) {
+        this.hasAudience = hasAudience;
+    }
+
+    public void updateHasQna(boolean hasQna) {
+        this.hasQna = hasQna;
+        if(this.hasQna){
+            this.questionCount = 1;
+            this.answerTimeLimit = 1;
+        }
+    }
+
+    public void updateQuestionCount(int questionCount) {
+        this.questionCount = questionCount;
+    }
+
+    public void updateAnswerTimeLimit(int answerTimeLimit) {
+        this.answerTimeLimit = answerTimeLimit;
     }
 }
