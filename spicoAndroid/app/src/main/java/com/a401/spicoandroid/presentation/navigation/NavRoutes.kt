187fa61c9e80b1c853e4fa777cfac2c7545cd3db
yejin <kimyejin0044@gmail.com>
2025-05-08 16:37:00 +0900

    🔧[S12P31A401-170] chore: Set up Android navigation component

--- spicoAndroid/app/src/main/java/com/a401/spicoandroid/presentation/navigation/NavRoutes.kt
+++ spicoAndroid/app/src/main/java/com/a401/spicoandroid/presentation/navigation/NavRoutes.kt
@@ -2,7 +2,10 @@ package com.a401.spicoandroid.presentation.navigation
 
 sealed class NavRoutes(val route: String) {
     object Main : NavRoutes("main")
-    object ProjectList : NavRoutes("project_list")
-    object ProjectDetail : NavRoutes("project_detail")
-    object Profile : NavRoutes("profile")
+
+    object ProjectCreate: NavRoutes("project_create")
+    object ProjectList: NavRoutes("project_list")
+    object ProjectDetail: NavRoutes("project_detail/{projectId}") {
+        fun withId(projectId: Int) = "project_detail/$projectId"
+    }
 }
