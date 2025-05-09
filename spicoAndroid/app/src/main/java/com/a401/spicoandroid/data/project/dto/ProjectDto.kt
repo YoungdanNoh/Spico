45f802d6d9f85056fb290e7053ab0462a2cb9cf6
yejin <kimyejin0044@gmail.com>
2025-05-09 16:06:55 +0900

    ✅[S12P31A401-117] test: Test communication with backend API

--- spicoAndroid/app/src/main/java/com/a401/spicoandroid/data/project/dto/ProjectDto.kt
+++ spicoAndroid/app/src/main/java/com/a401/spicoandroid/data/project/dto/ProjectDto.kt
@@ -10,18 +10,10 @@ data class ProjectDto(
     val projectDate: String
 )
 
-data class ProjectListResponse(
-    val projects: List<ProjectDto>
-)
-
 fun ProjectDto.toDomain(): Project {
     return Project(
         id = this.projectId,
         title = this.projectName,
         date = LocalDate.parse(this.projectDate, DateTimeFormatter.ISO_DATE)
     )
-}
-
-fun ProjectListResponse.toDomainList(): List<Project> {
-    return this.projects.map { it.toDomain() }
 }
