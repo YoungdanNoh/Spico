f6d2bc9497c2d209e1920d9c95609d18641bb639
yejin <kimyejin0044@gmail.com>
2025-05-13 12:52:31 +0900

    ✨[S12P31A401-87] feat: Implement get random speech list API

--- spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/repository/RandomSpeechRepositoryCustomImpl.kt
+++ spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/repository/RandomSpeechRepositoryCustomImpl.kt
@@ -1,4 +1,22 @@
 package com.ssafy.spico.domain.randomSpeech.repository
 
-class RandomSpeechRepositoryCustomImpl: RandomSpeechRepositoryCustom {
+import com.querydsl.jpa.impl.JPAQueryFactory
+import com.ssafy.spico.domain.randomSpeech.entity.QRandomSpeechEntity
+import com.ssafy.spico.domain.randomSpeech.entity.RandomSpeechEntity
+import org.springframework.stereotype.Repository
+
+@Repository
+class RandomSpeechRepositoryCustomImpl(
+    private val queryFactory: JPAQueryFactory
+): RandomSpeechRepositoryCustom {
+
+    override fun findRandomSpeechesByUserId(userId: Int): List<RandomSpeechEntity> {
+        val randomSpeech = QRandomSpeechEntity.randomSpeechEntity
+
+        return queryFactory.selectFrom(randomSpeech)
+            .where(randomSpeech.userEntity.id.eq(userId))
+            .orderBy(randomSpeech.createdAt.desc())
+            .fetch()
+
+    }
 }
