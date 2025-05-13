ce42c25931ecf022e2f95ec9049f679ddaaf0ca5
yejin <kimyejin0044@gmail.com>
2025-05-13 14:09:25 +0900

    ✨[S12P31A401-85] feat: Implement start random speech API

--- spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/controller/RandomSpeechController.kt
+++ spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/controller/RandomSpeechController.kt
@@ -1,11 +1,12 @@
 package com.ssafy.spico.domain.randomSpeech.controller
 
 import com.ssafy.spico.common.response.ApiResponse
-import com.ssafy.spico.domain.randomSpeech.dto.RandomSpeechResponseDto
-import com.ssafy.spico.domain.randomSpeech.dto.toResponse
+import com.ssafy.spico.domain.randomSpeech.dto.*
 import com.ssafy.spico.domain.randomSpeech.service.RandomSpeechService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -19,5 +20,14 @@ class RandomSpeechController(
     fun getRandomSpeeches(): ApiResponse<List<RandomSpeechResponseDto>> {
         val randomSpeeches = randomSpeechService.getRandomSpeechList(userId)
         return ApiResponse.success(randomSpeeches.map { it.toResponse() })
+    }
+
+    @PostMapping
+    fun createRandomSpeech(
+        @RequestBody request: CreateRandomSpeechRequestDto
+    ): ApiResponse<RandomSpeechContentResponseDto> {
+        val randomSpeech = request.toRandomSpeech(userId)
+        val content = randomSpeechService.startRandomSpeech(randomSpeech)
+        return ApiResponse.success(content.toResponse())
     }
 }
