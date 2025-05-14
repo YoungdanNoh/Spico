9e85a34310bfc466523b7117590d8688388834c4
yejin <kimyejin0044@gmail.com>
2025-05-14 13:38:31 +0900

    ✨[S12P31A401-91] feat: Implement news search logic

---
+++ spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/model/Topic.kt
@@ -0,0 +1,12 @@
+package com.ssafy.spico.domain.randomSpeech.model
+
+enum class Topic {
+    BUSINESS,
+    POLITICS,
+    TECHNOLOGY,
+    SCIENCE,
+    SPORTS,
+    WORLD,
+    ENVIRONMENT,
+    ENTERTAINMENT
+}
