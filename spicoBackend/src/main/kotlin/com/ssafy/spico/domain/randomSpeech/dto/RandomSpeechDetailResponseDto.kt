ed66f309e7533eee57fcf2ccef6d9e1d6e17283c
yejin <kimyejin0044@gmail.com>
2025-05-15 14:24:15 +0900

    ✨[S12P31A401-88] feat: Implement get random speech detail API

--- spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/dto/RandomSpeechDetailResponseDto.kt
+++ spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/dto/RandomSpeechDetailResponseDto.kt
@@ -1,5 +1,34 @@
 package com.ssafy.spico.domain.randomSpeech.dto
 
+import com.ssafy.spico.domain.randomSpeech.exception.RandomSpeechError
+import com.ssafy.spico.domain.randomSpeech.exception.RandomSpeechException
+import com.ssafy.spico.domain.randomSpeech.model.RandomSpeech
+import com.ssafy.spico.domain.randomSpeech.model.Topic
+
 data class RandomSpeechDetailResponseDto(
-    val id: Int,
+    val title: String,
+    val topic: Topic,
+    val date: String,
+    val question: String,
+    val newsTitle: String,
+    val newsUrl: String,
+    val newsSummary: String,
+    val feedback: String,
+    val script: String
 )
+
+fun RandomSpeech.toDetailResponse(): RandomSpeechDetailResponseDto {
+    this.report?: throw RandomSpeechException(RandomSpeechError.REPORT_NOT_FOUND)
+    this.content?: throw RandomSpeechException(RandomSpeechError.CONTENT_NOT_FOUND)
+    return RandomSpeechDetailResponseDto(
+        title = this.report.title,
+        topic = this.topic,
+        date = this.createdAt.toLocalDate().toString(),
+        question = this.content.question,
+        newsTitle = this.content.newsTitle,
+        newsUrl = this.content.newsUrl,
+        newsSummary = this.content.newsSummary,
+        feedback = this.report.feedback,
+        script = this.report.script,
+    )
+}
--- spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/exception/RandomSpeechError.kt
+++ spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/exception/RandomSpeechError.kt
@@ -8,5 +8,9 @@ enum class RandomSpeechError(
     USER_NOT_FOUND("404", "S001", "사용자를 찾을 수 없습니다."),
 
     SPEECH_NOT_FOUND("404", "S101", "랜덤스피치가 존재하지 않습니다."),
-    ALREADY_ENDED_SPEECH("404", "S102", "이미 종료된 랜덤스피치입니다.")
+    ALREADY_ENDED_SPEECH("404", "S102", "이미 종료된 랜덤스피치입니다."),
+    UNAUTHORIZED_ACCESS("403", "S103", "해당 랜덤스피치에 접근할 권한이 없습니다."),
+
+    REPORT_NOT_FOUND("404", "S201", "리포트를 찾을 수 없습니다."),
+    CONTENT_NOT_FOUND("404", "S202", "질문/뉴스를 찾을 수 없습니다."),
 }
