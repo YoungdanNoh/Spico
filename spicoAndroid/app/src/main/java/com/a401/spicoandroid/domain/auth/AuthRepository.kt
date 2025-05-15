2c779f789ab26b3f1956629b4df69592ef178b24
이승연 <snowball@sookmyung.ac.kr>
2025-05-15 17:21:16 +0900

    ✨[S12P31A401-217] feat: Implement Kakao login on Android
    # Conflicts:
    #	spicoAndroid/app/src/main/java/com/a401/spicoandroid/di/ApiModule.kt

--- spicoAndroid/app/src/main/java/com/a401/spicoandroid/domain/auth/AuthRepository.kt
+++ spicoAndroid/app/src/main/java/com/a401/spicoandroid/domain/auth/AuthRepository.kt
@@ -1,14 +1,14 @@
 package com.a401.spicoandroid.domain.auth
 
 import com.a401.spicoandroid.common.domain.DataResource
-import com.microsoft.cognitiveservices.speech.transcription.User
+import com.a401.spicoandroid.data.auth.dto.AuthResponse
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
-    fun login(token: String): Flow<DataResource<User>> // TODO: User 객체가 아니더라도 응답에 사용할 객체 구현 필요
-    fun logout() : Flow<DataResource<Unit>>
-
-    fun getLoginState(): Flow<DataResource<Boolean>>
-    suspend fun saveAccessToken(accessToken: String)
+    suspend fun login(kakaoToken: String): Flow<DataResource<AuthResponse>> // TODO: User 객체가 아니더라도 응답에 사용할 객체 구현 필요
+    suspend fun logout() : Flow<DataResource<Unit>>
+    suspend fun getLoginState(): Flow<DataResource<Boolean>>
     fun observeAccessToken(): Flow<String?>
+    fun observeNickname(): Flow<String?>
+    fun observeUserInfo(): Flow<AuthResponse>
 }
--- spicoAndroid/app/src/main/java/com/a401/spicoandroid/infrastructure/datastore/UserPreferences.kt
+++ spicoAndroid/app/src/main/java/com/a401/spicoandroid/infrastructure/datastore/UserPreferences.kt
@@ -1,46 +1,13 @@
 package com.a401.spicoandroid.infrastructure.datastore
 
-import android.content.Context
 import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
-import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class UserPreferences @Inject constructor(@ApplicationContext context: Context) {
-    private val Context.dataStore by preferencesDataStore("user_prefs")
-    private val dataStore = context.dataStore
 
-    companion object {
-        val LOGIN_STATE = booleanPreferencesKey("login_state")
-        val USER_NAME = stringPreferencesKey("user_name")
-        val USER_EMAIL = stringPreferencesKey("user_email")
-    }
-
-    suspend fun setLoginState(isLoggedIn: Boolean) {
-        dataStore.edit { prefs ->
-            prefs[LOGIN_STATE] = isLoggedIn
-        }
-    }
-
-    suspend fun setUserInfo(name: String, email: String) {
-        dataStore.edit { prefs ->
-            prefs[USER_NAME] = name
-            prefs[USER_EMAIL] = email
-        }
-    }
-
-    val userInfoFlow: Flow<UserInfo> = dataStore.data.map { prefs ->
-        UserInfo(
-            name = prefs[USER_NAME] ?: "김도영",
-            email = prefs[USER_EMAIL] ?: "doyeong@gmail.com"
-        )
-    }
+object UserPreferences {
+//    val LOGIN_STATE = booleanPreferencesKey("login_state")
+    val ACCESS_TOKEN = stringPreferencesKey("access_token")
+    val NICKNAME = stringPreferencesKey("nickname")
+    val EXPIRES_AT = longPreferencesKey("expires_at")
 }
-
-data class UserInfo(val name: String, val email: String)
