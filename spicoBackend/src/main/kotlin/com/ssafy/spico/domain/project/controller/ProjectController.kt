1354698d2150203509199d3f43e43c4921eac50d
이승연 <snowball@sookmyung.ac.kr>
2025-05-16 13:40:52 +0900

    ♻️[S12P31A401-233] refactor: Use @UserId annotation in controller
    # Conflicts:
    #	spicoBackend/src/main/kotlin/com/ssafy/spico/domain/randomSpeech/controller/RandomSpeechController.kt

--- spicoBackend/src/main/kotlin/com/ssafy/spico/domain/project/controller/ProjectController.kt
+++ spicoBackend/src/main/kotlin/com/ssafy/spico/domain/project/controller/ProjectController.kt
@@ -1,22 +1,22 @@
 package com.ssafy.spico.domain.project.controller
 
+import com.ssafy.spico.common.annotaion.UserId
 import com.ssafy.spico.common.response.ApiResponse
 import com.ssafy.spico.domain.project.dto.*
 import com.ssafy.spico.domain.project.service.ProjectService
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/projects")
 class ProjectController(
-    private val projectService: ProjectService,
-    @Value("\${user-id}") private val userId: Int
+    private val projectService: ProjectService
 ) {
     @GetMapping
     fun getProjects(
         @RequestParam cursor: Int?,
         @RequestParam size: Int,
         @RequestParam type: ProjectViewType,
+        @UserId userId: Int
     ): ApiResponse<List<ProjectResponseDto>> {
         val projects = projectService.getProjects(userId, cursor, size, type)
         return ApiResponse.success(projects.map { it.toResponse() })
@@ -24,7 +24,8 @@ class ProjectController(
 
     @PostMapping
     fun createProject(
-        @RequestBody request: CreateProjectRequestDto
+        @RequestBody request: CreateProjectRequestDto,
+        @UserId userId: Int
     ): ApiResponse<CreateProjectResponseDto> {
         val project = request.toProject(userId)
         val projectId = projectService.createProject(project)
@@ -34,7 +35,8 @@ class ProjectController(
     @PatchMapping("/{projectId}")
     fun updateProject(
         @PathVariable projectId: Int,
-        @RequestBody request: UpdateProjectRequestDto
+        @RequestBody request: UpdateProjectRequestDto,
+        @UserId userId: Int
     ): ApiResponse<Unit> {
         projectService.updateProject(projectId, request)
         return ApiResponse.success()
