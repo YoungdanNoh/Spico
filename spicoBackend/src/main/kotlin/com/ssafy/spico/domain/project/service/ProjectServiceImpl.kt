e07f5b3001e76e11dd4ff248b1d495cab5f6a07d
yejin <kimyejin0044@gmail.com>
2025-05-19 13:48:49 +0900

    🐛[S12P31A401-86] fix: fixed practice cnt error

--- spicoBackend/src/main/kotlin/com/ssafy/spico/domain/project/service/ProjectServiceImpl.kt
+++ spicoBackend/src/main/kotlin/com/ssafy/spico/domain/project/service/ProjectServiceImpl.kt
@@ -1,6 +1,14 @@
 package com.ssafy.spico.domain.project.service
 
+import com.ssafy.spico.domain.practice.dto.PracticeListResponseDto
+import com.ssafy.spico.domain.practice.dto.PracticeResponseDto
+import com.ssafy.spico.domain.practice.dto.toResponse
 import com.ssafy.spico.domain.practice.service.DeletePracticeService
+import com.ssafy.spico.domain.practice.entity.PracticeType
+import com.ssafy.spico.domain.practice.model.Practice
+import com.ssafy.spico.domain.practice.model.toModel
+import com.ssafy.spico.domain.practice.repository.CoachingReportsRepository
+import com.ssafy.spico.domain.practice.repository.FinalReportsRepository
 import com.ssafy.spico.domain.project.dto.ProjectViewType
 import com.ssafy.spico.domain.project.dto.UpdateProjectRequestDto
 import com.ssafy.spico.domain.project.dto.toCommand
@@ -18,7 +26,9 @@ import org.springframework.transaction.annotation.Transactional
 class ProjectServiceImpl(
     private val projectRepository: ProjectRepository,
     private val userRepository: UserRepository,
-    private val deletePracticeService: DeletePracticeService
+    private val deletePracticeService: DeletePracticeService,
+    private val coachingReportsRepository: CoachingReportsRepository,
+    private val finalReportsRepository: FinalReportsRepository
 ): ProjectService {
     override fun getProjects(userId: Int, cursor: Int?, size: Int, type: ProjectViewType): List<Project> {
 
@@ -72,5 +82,20 @@ class ProjectServiceImpl(
         val projectEntity = projectRepository.findById(projectId)
             .orElseThrow { ProjectException(ProjectError.PROJECT_NOT_FOUND) }
         return projectEntity.toModel()
+    }
+
+    override fun getPractices(
+        userId: Int,
+        projectId: Int,
+        practiceFilter: PracticeType?,
+        cursor: Int?,
+        size: Int
+    ): List<PracticeResponseDto> {
+        require(size >= 1) { throw ProjectException(ProjectError.INVALID_PAGE_SIZE) }
+
+        val practices = projectRepository.findPracticesByProjectIdWithPaging(userId, projectId, practiceFilter, cursor, size).map { it.toModel() }
+        return practices.map { practice ->
+            practice.toResponse(coachingReportsRepository, finalReportsRepository)
+        }
     }
 }
